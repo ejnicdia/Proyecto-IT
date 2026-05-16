4621b02adc98cf32951f92845ff0ec8946f2faa4
ramsesRolo <ramsesrolo2@gmail.com>
2026-05-16 19:06:40 +0200

    Modelo reporte #2

--- proyectoGrupo3/catalogoMusicos/models.py
+++ proyectoGrupo3/catalogoMusicos/models.py
@@ -2,48 +2,11 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=250)
-
-"""
-class Articulo(models.Model):
-    # Campos para título y cuerpo
-    titulo = models.CharField(max_length=250)
-    cuerpo = models.TextField()
-
-    # Campo para la URL amigable (ej: titulo-del-articulo)
-    slug = models.SlugField(max_length=250)
-
-    # Relación con el Autor (Usuario)
-    autor = models.ForeignKey(
-    settings.AUTH_USER_MODEL,
-    on_delete=models.CASCADE,
-    related_name='articulos_blog'
-    )
+class Reporte(models.Model):
     
-    publicado = models.DateTimeField(default=timezone.now)
-    creado = models.DateTimeField(auto_now_add=True)
-    actualizado = models.DateTimeField(auto_now=True)
-
-    class Estado(models.TextChoices):
-        BORRADOR = 'B', 'Borrador'
-        PUBLICADO = 'P', 'Publicado'
-
-    estado = models.CharField(
-        max_length=1,
-        choices=Estado.choices,
-        default=Estado.BORRADOR
-    )
-
-    class Meta:
-        ordering = ['-publicado'] # Orden descendente (más nuevos primero)
-        indexes = [
-        models.Index(fields=['-publicado']), # Índice para optimizar consultas
-        ]
-        # Opcional: Nombres legibles en el panel de administracion
-        verbose_name = 'Artículo'
-        verbose_name_plural = 'Artículos'
-
+    titulo = models.CharField(max_length=250)
+    descripcion_error = models.TextField(max_length=250)
+    fecha_creacion = models.DateTimeField(default=timezone.now)
+   
     def __str__(self):
         return self.titulo
-"""
