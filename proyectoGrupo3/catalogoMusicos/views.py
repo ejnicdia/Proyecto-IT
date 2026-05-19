2ef489603772b05ff126bf1dc15fd313905f73c6
LuisGL <luis.garcialinan@gmail.com>
2026-05-19 17:15:37 +0200

    Crear cabeceras de funciones de vistas para listar

--- proyectoGrupo3/catalogoMusicos/views.py
+++ proyectoGrupo3/catalogoMusicos/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def listar_musicos(request):
+    return None
+
+def listar_usuarios(request):
+    return None
+
+def listar_bandas(request):
+    return None
+
+def listar_anuncios(request):
+    return None
+
+def listar_reportes(request):
+    return None
+
+def listar_eventos(request):
+    return None
