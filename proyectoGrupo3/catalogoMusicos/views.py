69b017b63377568d7cea6c9142225aa99f16dffc
LuisGL <luis.garcialinan@gmail.com>
2026-05-19 17:17:21 +0200

    Añadir funciones de ejemplo de vistas

--- proyectoGrupo3/catalogoMusicos/views.py
+++ proyectoGrupo3/catalogoMusicos/views.py
@@ -19,3 +19,21 @@ def listar_reportes(request):
 
 def listar_eventos(request):
     return None
+
+"""
+def lista_articulos(request):
+    # Usas el manager personalizado 'publicados' que creaste en la EPD 02
+    articulos = Articulo.publicados.all()
+    return render(request,
+        'blog/articulo/lista.html',
+        {'articulos': articulos})# Vista para el detalle de un artículo
+
+def detalle_articulo(request, id):
+    # Se intenta recuperar el artículo por ID. Si no existe, lanza un error 404 automáticamente.
+    articulo = get_object_or_404(Articulo,
+        id=id,
+        estado=Articulo.Estado.PUBLICADO)
+    return render(request,
+        'blog/articulo/detalle.html',
+        {'articulo': articulo})
+"""
