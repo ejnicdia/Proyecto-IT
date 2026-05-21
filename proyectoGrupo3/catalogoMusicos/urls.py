5afe015429687f8fd8be2c051e9d904f26264197
ramsesRolo <ramsesrolo2@gmail.com>
2026-05-21 17:05:37 +0200

    templates de detalle de cada uno de los modelos

--- proyectoGrupo3/catalogoMusicos/urls.py
+++ proyectoGrupo3/catalogoMusicos/urls.py
@@ -7,8 +7,13 @@ app_name = 'catalogoMusicos'
 urlpatterns = [
     # Ruta para el listado (ej: /catalogoMusicos/listar)
     path('musicos/', views.listar_musicos, name='listar_musicos'),
+    path('musicos/<int:id>/', views.detalle_musico, name='detalle_musico'),
     path('bandas/', views.listar_bandas, name='listar_bandas'),
+    path('bandas/<int:id>/', views.detalle_banda, name='detalle_banda'),
     path('anuncios/', views.listar_anuncios, name='listar_anuncios'),
+    path('anuncios/<int:id>/', views.detalle_anuncio, name='detalle_anuncio'),
     path('eventos/', views.listar_eventos, name='listar_eventos'),
+    path('eventos/<int:id>/', views.detalle_evento, name='detalle_evento'),
     path('reportes/', views.listar_reportes, name='listar_reportes'),
+    path('reportes/<int:id>/', views.detalle_reporte, name='detalle_reporte'),
 ]
